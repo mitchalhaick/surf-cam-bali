9f4a9677bc0658e2491d2243864a33d5dbf9fb2e
Alex Titaev <alextitaev@MacBook-Air-Alex.local>
2026-06-24 15:33:32 +0800

    improve portfolio design

--- src/app/surf-drone/page.js
+++ src/app/surf-drone/page.js
@@ -128,7 +128,7 @@ export default function SurfDrone() {
         </section>
 
         <section id="contact">
-          <div className="bg-zinc-900 rounded-3xl p-10">
+          <div className="bg-zinc-900 rounded-3xl p-2 md:p-10">
 
   <div className="text-center text-zinc-500 text-sm tracking-[0.3em] mb-8">
     CONTACT
